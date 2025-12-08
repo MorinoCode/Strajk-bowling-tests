1f50426ad3ad1df3936ebdb51716a4395a2962ab
MorinoCode <mortezamastercode@gmail.com>
2025-12-08 11:18:39 +0100

    ErrorMessage component tests

--- src/components/ErrorMessage/ErrorMessage.test.jsx
+++ src/components/ErrorMessage/ErrorMessage.test.jsx
@@ -3,12 +3,14 @@ import ErrorMessage from "./ErrorMessage";
 
 describe("ErrorMessage Component", () => {
   it("renders the error message text", () => {
+    // Stödjer: VG-kriterier som kräver att ett felmeddelande visas.
     render(<ErrorMessage message="Something went wrong!" />);
 
     expect(screen.getByText("Something went wrong!")).toBeInTheDocument();
   });
 
   it("wraps message inside article with correct class", () => {
+    // Hjälptest: Kontrollerar korrekt DOM-struktur för styling.
     const { container } = render(<ErrorMessage message="Error here" />);
     
     const article = container.querySelector(".error-message");
